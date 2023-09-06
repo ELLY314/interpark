f430ffd916875e60bb6b8e024d9dc423a87818ca
ELLY314 <linilinilyn@gmail.com>
2023-09-06 18:08:57 +0900

    feat : header 및 오늘의 픽 완성
    header 반응형완성
    오늘의 픽 json데이터 연동 및 반응형 완성

---
+++ js/todaypick.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", function(){
+    let data;
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'data/todaypick.json');
+    xhttp.send();
+    xhttp.onreadystatechange = function (event) {
+        const req = event.target;
+        if (req.readyState === XMLHttpRequest.DONE) {
+            console.log(req.response);
+            data = JSON.parse(req.response);
+            todaypickSlide();
+        }
+    };
+    function todaypickSlide(){
+        let swTodaypickHtml = ``;
+        for (let i = 0; i < data.todaypick_count; i++) {
+          let obj = data[`todaypick_${i + 1}`];
+          let html = `
+            <li class="swiper-slide">
+                <a href="${obj.link}">
+                    <img src="img/${obj.img}" alt="${obj.name}" />
+                </a>
+            </li>
+          `;
+          swTodaypickHtml += html;
+        }
+        let swTodaypickWrapper = document.querySelector('.sw-todaypick .swiper-wrapper');
+        swTodaypickWrapper.innerHTML = swTodaypickHtml;
+
+        var swiper = new Swiper(".sw-todaypick", {
+            slidesPerView: 1,
+            spaceBetween: 25,
+            loop:true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".todaypick .sw-next",
+                prevEl: ".todaypick .sw-prev",
+            },
+            pagination: {
+                el: '.sw-todaypick-pg',
+                clickable: true,
+            },
+            breakpoints: {
+                760: {
+                  slidesPerView: 2,
+                },
+            },
+        });
+    }
+    
+})
