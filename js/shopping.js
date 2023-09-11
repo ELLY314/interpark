f7d6fdb35fe0dd04693410a3fb49676e3650eae0
ELLY314 <linilinilyn@gmail.com>
2023-09-11 18:11:09 +0900

    feat : 쇼핑추천 완료, 투어 작업중
    쇼핑추천 탭 추가 완료, 반응형 수정 필요
    투어 json 연동 완료 반응형 작업 필요

--- js/shopping.js
+++ js/shopping.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function(){
         data = _data;
         let tabHtml = ``;
         let dataArr = _data.shopping;
+        console.log(_data)
         for (let i = 0; i < dataArr.length; i++){
             let html = `<li><a href="#">${dataArr[i].catename}</a></li>`
             tabHtml += html;
@@ -18,7 +19,8 @@ window.addEventListener("load", function(){
 
         let tabs = document.querySelectorAll(".shopping .slide-tabs li")
         tabs.forEach(function(item, index){
-            item.addEventListener("click", function(){
+            item.addEventListener("click", function(event){
+                event.preventDefault();
                 shoppingSlide(index);
                 tabs.forEach((item)=> item.classList.remove("active"));
                 this.classList.add("active");
@@ -43,7 +45,10 @@ window.addEventListener("load", function(){
                             <img src="${obj.img}" alt="${obj.alt}">
                         </div>
                         <div class="textbox">
-                            <h2><span>${obj.ratio}</span>${obj.price}원</h2>
+                            <h2>
+                                <span class="ratio" ${obj.ratio ? "style='display:inline-block'" : "style='display:none'"}>${obj.ratio}%</span>
+                                <span>${obj.price}</span>원
+                            </h2>
                             <p>${obj.name}</p>
                         </div>
                     </a>
@@ -51,18 +56,18 @@ window.addEventListener("load", function(){
             `
             swShoppingHtml += html;
         }
-        swShoppingWrap = document.querySelector(".sw-shopping .swiper-wrapper");
+        let swShoppingWrap = document.querySelector(".sw-shopping .swiper-wrapper");
         swShoppingWrap.innerHTML = swShoppingHtml;
         if(shoppingSwiper){
             shoppingSwiper.destroy();
         }
         shoppingSwiper = new Swiper(".sw-shopping", {
-            // spaceBetween: 10,
-            // slidesPerView: 5,
-            // grid:{
-            //     rows:2,
-            //     fill:'row',
-            // },
+            spaceBetween: 10,
+            slidesPerView: 4,
+            grid:{
+                rows:2,
+                fill:'row',
+            },
             navigation: {
                 nextEl: ".shopping .sw-next",
                 prevEl: ".shopping .sw-prev",
@@ -76,65 +81,9 @@ window.addEventListener("load", function(){
                 1024: {
                     slidesPerView: 4,
                     slidesPerGroup:4,
+                    spaceBetween: 25,
                 },
             },
         });
-
-
     }
-
-    // let data;
-    // const xhttp = new XMLHttpRequest();
-    // xhttp.open('GET', 'data/shopping.json');
-    // xhttp.send();
-    // xhttp.onreadystatechange = function (event) {
-    //     const req = event.target;
-    //     if (req.readyState === XMLHttpRequest.DONE) {
-    //         data = JSON.parse(req.response);
-    //         shoppingSlide();
-    //     }
-    // };
-    // function shoppingSlide(_idx){
-    //     let swShoppingHtml = ``;
-    //     let listData = data.shopping[_idx].list;
-    //     for(let i = 0; i < listData.length; i++){
-    //         let obj = listData[i];
-    //         let html = `
-    //             <li class="swiper-slide">
-    //                 <a href="${obj.link}">
-    //                     <div class="imgbox">
-    //                         <img src="${obj.img}" alt="${obj.alt}">
-    //                     </div>
-    //                     <div class="textbox">
-    //                         <h2><span>${obj.ratio}</span>${obj.price}원</h2>
-    //                         <p>${obj.name}</p>
-    //                     </div>
-    //                 </a>
-    //             </li>
-    //         `;
-    //         swShoppingHtml+=html;
-    //     }
-    //     let swShoppingWrap = document.querySelector(".sw-shopping .swiper-wrapper");
-    //     swShoppingWrap.innerHTML = swShoppingHtml;
-
-    //     var swiper = new Swiper(".sw-shopping", {
-    //         spaceBetween: 25,
-    //         navigation: {
-    //             nextEl: ".shopping .sw-next",
-    //             prevEl: ".shopping .sw-prev",
-    //         },
-    //         breakpoints: {
-    //             760: {
-    //               slidesPerView: 3,
-    //               slidesPerGroup:3,
-    //             },
-    //             1024: {
-    //               slidesPerView: 4,
-    //               slidesPerGroup:4,
-    //             },
-    //         },
-    //     });
-    // }
-
-    
 })
---
+++ js/tour.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", function(){
+    this.fetch('data/tour.json')
+        .then((res) => res.json())
+        .then((result) => parseTour(result))
+        .catch((err) => console.log(err));
+    let data;
+    function parseTour(_data){
+        data=_data;
+        let tabs = document.querySelectorAll(".tour .slide-tabs li")
+        tabs.forEach(function(item, index){
+            item.addEventListener("click", function(event){
+                event.preventDefault();
+                tourSlide(index);
+                tabs.forEach((item)=> item.classList.remove("active"));
+                this.classList.add("active");
+            })
+            tabs[0].classList.add("active");
+            tourSlide(0);
+        })
+    }
+    let tourSwiper;
+
+    function tourSlide(_idx){
+        let swTourHtml = ``;
+        let tourData = data.tour[_idx].list;
+        for(let i = 0; i < tourData.length; i++){
+            let obj = tourData[i];
+            let html = `
+                <li class="swiper-slide">
+                    <a href="${obj.link}">
+                        <div class="imgbox">
+                            <img src="${obj.img}" alt="${obj.alt}">
+                        </div>
+                        <div class="textbox">
+                            <span class="point" ${obj.point ? "style='display:block'" : "style='display:none'"}>${obj.point}</span>
+                            <p class="promotion" ${obj.promotion ? "style='display:block'" : "style='display:none'"}>${obj.promotion}</p>
+                            <p class="title">${obj.title}</p>
+                            <p class="price"><b>${obj.price}</b>원~</p>
+                        </div>
+                    </a>
+                </li>
+            `
+            swTourHtml += html
+        }
+        let swTourWrap = document.querySelector(".sw-tour .swiper-wrapper");
+        swTourWrap.innerHTML = swTourHtml;
+        if(tourSwiper){
+            tourSwiper.destroy();
+        }
+        tourSwiper = new Swiper(".sw-tour", {
+            navigation: {
+                nextEl: ".tour .sw-next",
+                prevEl: ".tour .sw-prev",
+            },
+            breakpoints: {
+                760: {
+                    slidesPerView: 2,
+                    slidesPerGroup:2,
+                    spaceBetween: 25,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    slidesPerGroup:3,
+                    spaceBetween: 25,
+                },
+            },
+        });
+    }
+    
+})
